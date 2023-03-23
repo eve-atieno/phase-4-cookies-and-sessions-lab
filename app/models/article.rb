b910e02f9a86bdadb21e9e3bf309ac595a66e5a9
eve-atieno <atienoevelyne53@gmail.com>
2023-03-23 18:15:26 +0300

    completed assignment

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,4 +13,15 @@ class Article < ApplicationRecord
     created_at.strftime('%B %e, %Y')
   end
 
+  class CreateArticles < ActiveRecord::Migration[6.1]
+    def change
+      create_table :articles do |t|
+        t.string :title
+        t.text :content
+        t.timestamps
+      end
+    end
+  end
+  
+
 end
